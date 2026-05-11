8517fa2e3d2b50dfca2524ea0f49a3facfe08635
Phil Karn <karn@ka9q.net>
2026-05-10 17:49:23 -0700

    starting debian packaging

---
+++ debian/README.source.ex
@@ -0,0 +1,9 @@
+wwvsim for Debian
+----------------
+
+<This file describes information about the source package, see Debian policy
+manual section 4.14. You WILL either need to modify or delete this file.>
+
+
+
+ -- Phil Karn <karn@ka9q.net>  Sun, 10 May 2026 17:48:16 -0700
---
+++ debian/upstream.ex/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/wwvsim/issues
+# Bug-Submit: https://github.com/<user>/wwvsim/issues/new
+# Changelog: https://github.com/<user>/wwvsim/blob/master/CHANGES
+# Documentation: https://github.com/<user>/wwvsim/wiki
+# Repository-Browse: https://github.com/<user>/wwvsim
+# Repository: https://github.com/<user>/wwvsim.git
